ad2a46464670351e95d44954603afe68092b3396
brhelm <brhelm77@gmail.com>
2018-02-16 12:42:15 -0800

    formatting

--- src/components/Book1.js
+++ src/components/Book1.js
@@ -14,9 +14,10 @@ class Book1 extends Component {
                 <br />
                 <div className="container">
                     <div className='row'>
+                        <span>Title: My Life </span>
                         <div className='col-sm-3 col-md-6 col-lg-12'>
                             <FlipPage orientation="horizontal" >
-                            
+
                                 <img className="" src={require("../img/newstory1.png")} style={{ height: '480px', width: '240px' }} />
                                 <img className="" src={require("../img/popstory1.jpg")} style={{ height: '480px', width: '240px' }} />
                                 <img className="" src={require("../img/book2.jpg")} style={{ height: '480px', width: '240px' }} />
@@ -24,6 +25,7 @@ class Book1 extends Component {
 
                             </FlipPage>
                         </div>
+                        <div><span>...all those memories, moments in time... lost, like tears in the rain... </span></div>
                     </div>
                 </div>
                 <br />
