ddc74996d41d7da813d8da8943cbeb4b91378a7b
Josiah De La Torre <jodelat@yahoo.com>
2018-02-19 11:40:17 -0800

    fixed books sitting on shelf and removed logo on splash

--- src/components/landing-components/splashpage.js
+++ src/components/landing-components/splashpage.js
@@ -2,18 +2,11 @@ import React from 'react';
 import '../../App.css';
 const Splash = () => {
   return (<div className="container-fluid">
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
     <div className="row">
       <div className="jumbophoto">
         <div className="col-sm-8 col-md-8 col-lg-8">
           <div className="row">
             <div className="col-sm-12 col-md-12 col-lg-12">
-              <img src={require('../../img/mysLogo00.png')} alt="xxxx"/>
-
             </div>
           </div>
           <div className="row">
