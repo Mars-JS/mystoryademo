a48214c549d8c64004fce2f083a485eb787cb40e
Josiah De La Torre <jodelat@yahoo.com>
2018-02-28 22:09:29 -0800

    added Create.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import ViewProfile from "./components/ViewProfile";
 import Book1 from "./components/Book1";
 import Book2 from "./components/Book2";
 import BookShelf from "./components/BookShelf";
+import Create from "./components/Create";
 /* const Header = 'header'; */
 /* const Book1 = 'book1'; */
 /* const Book2 = 'book2'; */
@@ -27,6 +28,7 @@ class App extends Component {
           <Route exact path="/books/1" component={Book1} />
           <Route exact path="/books/2" component={Book2} />
           <Route exact path="/books" component={Landing} />
+          <Route exact path="/create" component={Create} />
           <Footer/>
       </div>
     </BrowserRouter>
