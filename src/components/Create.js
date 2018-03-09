8747b40b8e00463150565dd00db1bd711ac259a5
brhelm <brhelm77@gmail.com>
2018-03-08 18:47:09 -0800

    another shelf

--- src/components/Create.js
+++ src/components/Create.js
@@ -204,7 +204,7 @@ class Create extends Component {
         </div>
 
         <div className="row"><div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
-          <BookShelfControl/>
+          
           <BookShelfNew />
         </div></div>
 
