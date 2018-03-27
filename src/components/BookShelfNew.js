089b9843148829d56d168eabe4a30c4ec77337e2
brhelm <brhelm77@gmail.com>
2018-03-27 11:20:37 -0700

    bookshelf updates

--- src/components/BookShelfNew.js
+++ src/components/BookShelfNew.js
@@ -23,13 +23,12 @@ class BookShelfNew extends Component {
 
     render() {
         const { index, direction } = this.state;
-
         return (
             <div className="container-fluid" >
                 <div className="row" style={{ marginTop: '1%' }}>
 
                     <div className="container">
-                        <div className="row titleRow">
+                        <div className="row titleRow"><a href='' className='viewAll'>View all ></a>
                             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                                 <div className="font3 shelfTitle">Popular Stories</div>
                             </div>
@@ -44,9 +43,14 @@ class BookShelfNew extends Component {
                                         <div className="row" style={{ marginLeft: '2%' }}>
                                             <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                                                 <div className='containerBook'>
-                                                    <img alt="" className="shelfBook2 img-responsive" src={require("../img/bookProgress.png")} />
+                                                    <img alt="" className="shelfBook2 img-responsive" src={require("../img/bookSports1.png")} />
                                                     <div className='overlay'>
                                                         <div className="bookTitle">book Title</div>
+                                                        <div className="bookViews"><span className='glyphicon glyphicon-sunglasses'/> 2,222</div>
+                                                        <div className="bookLikes"><span className='glyphicon glyphicon-heart'/> 2,222</div>
+                                                        <div className="bookComments"><span className='glyphicon glyphicon-comment'/> 2,222</div>
+                                                        <div className="bookRating"><span className='glyphicon glyphicon-star' /><span className='glyphicon glyphicon-star' /><span className='glyphicon glyphicon-star' />
+                                                            <span className='glyphicon glyphicon-star' /><span className='glyphicon glyphicon-star-empty' /></div>
                                                         <div className="bookAuthor">book Author</div>
                                                     </div>
                                                 </div>
@@ -56,6 +60,11 @@ class BookShelfNew extends Component {
                                                     <img alt="" className="shelfBook2 img-responsive" src={require("../img/bookProgress.png")} />
                                                     <div className='overlay'>
                                                         <div className="bookTitle">book Title</div>
+                                                        <div className="bookViews"><span className='glyphicon glyphicon-sunglasses' /> 3,333</div>
+                                                        <div className="bookLikes"><span className='glyphicon glyphicon-heart' /> 3,333</div>
+                                                        <div className="bookComments"><span className='glyphicon glyphicon-comment' /> 3,333</div>
+                                                        <div className="bookRating"><span className='glyphicon glyphicon-star' /><span className='glyphicon glyphicon-star' /><span className='glyphicon glyphicon-star' />
+                                                            <span className='glyphicon glyphicon-star-empty' /><span className='glyphicon glyphicon-star-empty' /></div>
                                                         <div className="bookAuthor">book Author</div>
                                                     </div>
                                                 </div>
@@ -65,38 +74,11 @@ class BookShelfNew extends Component {
                                                     <img alt="" className="shelfBook2 img-responsive" src={require("../img/bookProgress.png")} />
                                                     <div className='overlay'>
                                                         <div className="bookTitle">book Title</div>
-                                                        <div className="bookAuthor">book Author</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div></div>
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <div className="container">
-                                        <div className="row" style={{ marginLeft: '2%' }}>
-                                            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                                                <div className='containerBook'>
-                                                    <img alt="" className="shelfBook2 img-responsive" src={require("../img/bookProgress.png")} />
-                                                    <div className='overlay'>
-                                                        <div className="bookTitle">book Title</div>
-                                                        <div className="bookAuthor">book Author</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                                                <div className='containerBook'>
-                                                    <img alt="" className="shelfBook2 img-responsive" src={require("../img/bookProgress.png")} />
-                                                    <div className='overlay'>
-                                                        <div className="bookTitle">book Title</div>
-                                                        <div className="bookAuthor">book Author</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                                                <div className='containerBook'>
-                                                    <img alt="" className="shelfBook2 img-responsive" src={require("../img/bookProgress.png")} />
-                                                    <div className='overlay'>
-                                                        <div className="bookTitle">book Title</div>
+                                                        <div className="bookViews"><span className='glyphicon glyphicon-sunglasses' /> 1,111</div>
+                                                        <div className="bookLikes"><span className='glyphicon glyphicon-heart' /> 1,111</div>
+                                                        <div className="bookComments"><span className='glyphicon glyphicon-comment' /> 1,111</div>
+                                                        <div className="bookRating"><span className='glyphicon glyphicon-star' /><span className='glyphicon glyphicon-star' /><span className='glyphicon glyphicon-star' />
+                                                            <span className='glyphicon glyphicon-star' /><span className='glyphicon glyphicon-star-empty' /></div>
                                                         <div className="bookAuthor">book Author</div>
                                                     </div>
                                                 </div>
@@ -108,9 +90,14 @@ class BookShelfNew extends Component {
                                         <div className="row" style={{ marginLeft: '2%' }}>
                                             <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                                                 <div className='containerBook'>
-                                                    <img alt="" className="shelfBook2 img-responsive" src={require("../img/bookProgress.png")} />
+                                                    <img alt="" className="shelfBook2 img-responsive" src={require("../img/bookSports1.png")} />
                                                     <div className='overlay'>
                                                         <div className="bookTitle">book Title</div>
+                                                        <div className="bookViews"><span className='glyphicon glyphicon-sunglasses' /> 2,222</div>
+                                                        <div className="bookLikes"><span className='glyphicon glyphicon-heart' /> 2,222</div>
+                                                        <div className="bookComments"><span className='glyphicon glyphicon-comment' /> 2,222</div>
+                                                        <div className="bookRating"><span className='glyphicon glyphicon-star' /><span className='glyphicon glyphicon-star' /><span className='glyphicon glyphicon-star' />
+                                                            <span className='glyphicon glyphicon-star' /><span className='glyphicon glyphicon-star-empty' /></div>
                                                         <div className="bookAuthor">book Author</div>
                                                     </div>
                                                 </div>
@@ -120,6 +107,11 @@ class BookShelfNew extends Component {
                                                     <img alt="" className="shelfBook2 img-responsive" src={require("../img/bookProgress.png")} />
                                                     <div className='overlay'>
                                                         <div className="bookTitle">book Title</div>
+                                                        <div className="bookViews"><span className='glyphicon glyphicon-sunglasses' /> 2,222</div>
+                                                        <div className="bookLikes"><span className='glyphicon glyphicon-heart' /> 2,222</div>
+                                                        <div className="bookComments"><span className='glyphicon glyphicon-comment' /> 2,222</div>
+                                                        <div className="bookRating"><span className='glyphicon glyphicon-star' /><span className='glyphicon glyphicon-star' /><span className='glyphicon glyphicon-star' />
+                                                            <span className='glyphicon glyphicon-star' /><span className='glyphicon glyphicon-star-empty' /></div>
                                                         <div className="bookAuthor">book Author</div>
                                                     </div>
                                                 </div>
@@ -129,6 +121,11 @@ class BookShelfNew extends Component {
                                                     <img alt="" className="shelfBook2 img-responsive" src={require("../img/bookProgress.png")} />
                                                     <div className='overlay'>
                                                         <div className="bookTitle">book Title</div>
+                                                        <div className="bookViews"><span className='glyphicon glyphicon-sunglasses' /> 2,222</div>
+                                                        <div className="bookLikes"><span className='glyphicon glyphicon-heart' /> 2,222</div>
+                                                        <div className="bookComments"><span className='glyphicon glyphicon-comment' /> 2,222</div>
+                                                        <div className="bookRating"><span className='glyphicon glyphicon-star' /><span className='glyphicon glyphicon-star' /><span className='glyphicon glyphicon-star' />
+                                                            <span className='glyphicon glyphicon-star' /><span className='glyphicon glyphicon-star-empty' /></div>
                                                         <div className="bookAuthor">book Author</div>
                                                     </div>
                                                 </div>
--- src/components/landing/SplashPage.js
+++ src/components/landing/SplashPage.js
@@ -19,29 +19,33 @@ const Splash = () => {
           </div>  */}
 
           
-          <form className="formPad">
-            <fieldset>
+
                 
-                <div id="row10c" className="row" style={{ marginBottom: '1%', marginLeft: '30%', marginTop: '70%' }}>
-                  <div className="col-sm-8 col-md-8 col-lg-8">
-                    <button className="loginBtn loginBtn--custom"><a className="loginbtn" href="/profile">Login with email</a></button>
-                  </div>
-                </div>
+          <div id="row10a" className="row" style={{ marginBottom: '1%', marginLeft: '30%', marginTop: '105%' }}>
+            <div className="col-sm-8 col-md-8 col-lg-8">
+              <button className="loginBtn loginBtn--custom"><a className="loginbtn" href="/profile">Login with email</a></button>
+            </div>
+          </div>
 
-                <div id="row10a" className="row" style={{ marginBottom: '1%', marginLeft: '30%'}}>
-                <div className="col-sm-8 col-md-8 col-lg-8">
-                  <button className="loginBtn loginBtn--facebook"><a className="loginbtn" href="/profile">Login with Facebook</a></button>
-                </div>
-              </div>
-              
-                <div id="row10b" className="row" style={{ marginBottom: '2%', marginLeft: '30%' }}>
-                <div className="col-sm-8 col-md-8 col-lg-8">
-                  <button className="loginBtn loginBtn--google"><a className="loginbtn" href="/profile">Login with Google</a></button>
-                </div>
-              </div>
+            <div id="row10b" className="row" style={{ marginBottom: '1%', marginLeft: '30%'}}>
+            <div className="col-sm-8 col-md-8 col-lg-8">
+              <button className="loginBtn loginBtn--facebook"><a className="loginbtn" href="/profile">Login with Facebook</a></button>
+            </div>
+          </div>
+          
+            <div id="row10c" className="row" style={{ marginBottom: '2%', marginLeft: '30%' }}>
+            <div className="col-sm-8 col-md-8 col-lg-8">
+              <button className="loginBtn loginBtn--google"><a className="loginbtn" href="/profile">Login with Google</a></button>
+            </div>
+          </div>
 
-            </fieldset>
-          </form>
+          <div id="row10d" className="row" style={{ marginBottom: '2%', marginLeft: '30%' }}>
+            <div className="col-sm-8 col-md-8 col-lg-8">
+              <div className='col-xs-2 col-sm-2 col-md-2 col-lg-12 platformButton'>OUR PLATFORM</div>
+            </div>
+          </div>
+            
+
           
 
         </div>
